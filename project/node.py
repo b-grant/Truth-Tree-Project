24a9f1d7cad50c1f9599a89cb5ac29ed958a3979
b-grant <57621440+b-grant@users.noreply.github.com>
2020-06-18 02:50:22 +0100

    Update node.py

--- project/node.py
+++ project/node.py
@@ -1,12 +1,17 @@
 class Node:
     def __init__(self, val):
+        #left node
         self.l = None
+        #right node
         self.r = None
+        #parent node
         self.p = None
         #v is the list of sentences present at that node
         self.v = val
         #vd is the list of sentences that should be printed at a node
         #this prevents repetition of sentences that are being carried down a path
         self.vd = []
+        #boolean for whether a path is done
         self.d = False
+        #boolean for whether there is a contradiction
         self.c = False
